d39645757193faeea585a3b85170a115e63557ce
Bixbi <bixby_358@hotmail.com>
2021-11-13 09:38:22 -0500

    Mostrar toda las peliculas en la ventana

--- src/data.js
+++ src/data.js
@@ -1,9 +1,13 @@
-// estas funciones son de ejemplo
+import data from './data/ghibli/ghibli.js';
 
-export const example = () => {
-  return 'example';
-};
 
-export const anotherExample = () => {
-  return 'OMG';
+/*
+filterData(data, condition): esta función filter o filtrar recibiría la data, 
+y nos retornaría aquellos datos que sí cumplan con la condición.
+*/
+export const filterData = (descripcion, condition) => {
+  let newArr = data.films.filter(function (newArray) {
+    return (newArray[descripcion] === condition);
+  });
+  return newArr;
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js';
+import {
 
-console.log(example, data);
+
+} from './data.js';
+
+
+const filmsData = data.films;
+const containerPokemons = document.querySelector('#containerFilms');
+
+
+
+
+const filmsList = (list) => {
+
+    list.forEach((ghibli) => {
+        const frameFilms = document.createElement('div');
+        frameFilms.className = 'filmsGroup';
+        frameFilms.innerHTML = `<div class="detalle-films">
+                                 <img src="${ghibli.poster}">
+                                 <hr width=210  size=10 color="#FFF">
+                                 <p class="title" >${ghibli.title}</p>
+                                 <p class="title" >${ghibli.director}</p>
+
+                                 </div>`;
+
+        containerPokemons.appendChild(frameFilms);
+    });
+};
+
+filmsList(filmsData);
