784ac24843ce5868339b1258c109292b39e6b573
Bixbi <bixby_358@hotmail.com>
2021-11-15 10:16:00 -0500

    Funcion filtrar por titulo

--- src/data.js
+++ src/data.js
@@ -5,9 +5,18 @@ import data from './data/ghibli/ghibli.js';
 filterData(data, condition): esta función filter o filtrar recibiría la data, 
 y nos retornaría aquellos datos que sí cumplan con la condición.
 */
-export const filterData = (descripcion, condition) => {
-  let newArr = data.films.filter(function (newArray) {
-    return (newArray[descripcion] === condition);
+export const filterData = (description, input) => {
+  const newArr = data.films.filter(function (newArray) {
+    return (newArray[description] === input);
   });
   return newArr;
 };
+
+
+export const searchData = (arrayObj, input) => {
+
+  const array = arrayObj.filter(pre => pre.title.toLowerCase().startsWith(input.toLowerCase()));
+  return array;
+};
+
+
--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import data from './data/ghibli/ghibli.js';
 import {
+    
+    filterData, 
+    searchData,
+    
 
 
 } from './data.js';
 
-
 const filmsData = data.films;
 const containerFilms = document.querySelector('#containerFilms');
-
-
-
+const inputSearch = document.getElementById('searchFilms');
 
 const filmsList = (list) => {
 
@@ -18,14 +19,20 @@ const filmsList = (list) => {
         frameFilms.className = 'filmsGroup';
         frameFilms.innerHTML = `<div class="detalle-films">
                                  <img src="${ghibli.poster}">
-                                 <hr width=210  size=10 color="#FFF">
+                                 <hr width=210  size=7 color="#FFF">
                                  <p class="title" >${ghibli.title}</p>
-                                 <p class="title" >${ghibli.director}</p>
+                                 <p class="title" >DIRECTOR: ${ghibli.director}</p>
 
                                  </div>`;
-
         containerFilms.appendChild(frameFilms);
     });
 };
 
 filmsList(filmsData);
+
+inputSearch.addEventListener('input', () =>{
+    const newDataFilter = searchData(filmsData, inputSearch.value);
+        containerFilms.innerHTML = '';
+        filmsList(newDataFilter);
+});
+
